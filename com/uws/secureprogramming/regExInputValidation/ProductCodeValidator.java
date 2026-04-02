58f761c458e5320e67e7ae29eae055e86fab5de4
Dean Cain <b01787017@studentmail.uws.ac.uk>
2026-04-02 09:56:12 +0100

    Exercise 2: Task 2: Fix regex pattern in ProductCodeValidator to enforce uppercase letters and numeric format for product codes

--- com/uws/secureprogramming/regExInputValidation/ProductCodeValidator.java
+++ com/uws/secureprogramming/regExInputValidation/ProductCodeValidator.java
@@ -2,7 +2,7 @@ package com.uws.secureprogramming.regExInputValidation;
 
 public class ProductCodeValidator implements iProductValidator {
 
-    private static final String PRODUCT_CODE_REGEX = "^[A-Za-z0-9]{3}-[A-Za-z0-9]{4}$$";
+    private static final String PRODUCT_CODE_REGEX = "^[A-Z]{3}-[0-9]{4}$";
 
     @Override
     public boolean isValidProductCode(String productCode) {
